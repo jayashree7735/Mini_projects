3877a40a1e09b3a09ea69b7600fb31aee4495efd
Jayashree Rout <jrout911@gmail.com>
2024-10-29 20:00:24 +0530

    Slider_image

---
+++ Slider_image/index.js
@@ -0,0 +1,20 @@
+function firstImage(){
+    document.getElementById("imgone").src="Motivational_img.png";
+}
+setInterval(firstImage,3000);
+
+function secondImage(){
+    document.getElementById("imgtwo").src="mtvl_imag1";
+}
+setInterval(secondImage,6000);
+
+function thirdImage(){
+    document.getElementById("imgthree").src="mtvl_img";
+}
+setInterval(thirdImage,9000);
+
+function fourImage(){
+    document.getElementById("imgfour").src="mtvl_img2";
+}
+setInterval(fourImage,9000);
+
