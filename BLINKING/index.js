b0259ad3b183e815d51c1638fd3365c9ec630535
Jayashree Rout <jrout911@gmail.com>
2024-10-29 19:55:47 +0530

    Blinking

---
+++ BLINKING/index.js
@@ -0,0 +1,16 @@
+setInterval(function blink(){
+document.getElementsByClassName("container").style = "color:blue";
+},250)
+
+setInterval(function blink(){
+    document.getElementById("container").style = "color:red";
+    },500)
+
+setInterval(function blink(){
+        document.getElementById("container").style = "color:purple";
+     },750)
+
+
+ setInterval(function blink(){
+        document.getElementById("container").style = "color:green";
+     },1000)
