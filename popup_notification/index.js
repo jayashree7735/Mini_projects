a886b0933f45dba499f15583ee0818fae724662c
Jayashree Rout <jrout911@gmail.com>
2024-10-29 19:58:05 +0530

    popup_notification

---
+++ popup_notification/index.js
@@ -0,0 +1,11 @@
+let button = document.getElementById('btn')
+
+let popop =document.getElementById("pop");
+
+function popon(){
+    popop.classList.add("open-popup");
+}
+function popoff(){
+    popop.classList.remove("open-popup");
+}
+
