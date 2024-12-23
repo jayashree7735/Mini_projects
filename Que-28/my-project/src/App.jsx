659f3195215d9bcde9eafc95e7990667703f1640
Jayashree Rout <jrout911@gmail.com>
2024-12-23 13:30:50 +0530

    28-answer

---
+++ Que-28/my-project/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+import React from "react";
+
+function Greeting() {
+  return <h1>Hello, Jayashree!</h1>;
+}
+
+export default Greeting;
+
---
+++ Que-28/my-project/src/Components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "./Hello";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Jayashree" />
+    </div>
+  );
+}
+
+export default App;
